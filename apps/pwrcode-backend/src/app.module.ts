c17b5dffa6670ee6f1c21b2f18ea34ff48807d67
IMHOJEONG <39ghwjd@naver.com>
2023-11-11 16:32:51 +0900

    [add]: add entity for pwrcode-backend

--- apps/pwrcode-backend/src/app.module.ts
+++ apps/pwrcode-backend/src/app.module.ts
@@ -2,20 +2,31 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArticlesModule } from './articles/articles.module';
+import { NewsModule } from './news/news.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        '.env.development',
+        '.env.production'
+      ]
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '172.17.0.1',
+      host:  process.env.DATABASE_HOST,
       port: 3306,
-      username: 'root',
-      password: 'root',
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASS,
       database: 'test',
       entities: [],
       synchronize: true,
     }),
-
+    
+    ArticlesModule,
+    NewsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
