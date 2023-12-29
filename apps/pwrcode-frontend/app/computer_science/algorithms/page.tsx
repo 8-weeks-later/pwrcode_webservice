871a1b4c512a7d27a5e8f67a8e18414018afbfa1
IMHOJEONG <39ghwjd@naver.com>
2023-12-30 01:23:31 +0900

    [revise]: revise page.tsx for fetch error

--- apps/pwrcode-frontend/app/computer_science/algorithms/page.tsx
+++ apps/pwrcode-frontend/app/computer_science/algorithms/page.tsx
@@ -7,26 +7,31 @@ import { Code } from 'bright';
 // app/page.js
 export default async function RemoteMdxPage() {
   // MDX text - can be from a local file, database, CMS, fetch, anywhere...
-  const res = await fetch(
-    'http://localhost:9000/computer-science/algorithms_and_data_structures/index.mdx',
-    {
-      headers: {
-        'Content-Type': 'text/html',
+  try {
+    const res = await fetch(
+      `http://${process.env.NEXT_PUBLIC_FILE_API}/computer-science/algorithms_and_data_structures/index.mdx`,
+      {
+        headers: {
+          'Content-Type': 'text/html',
+        },
       },
-    },
-  );
-  const jsonData = await res.text();
+    );
 
-  return (
-    <Suspense fallback={<>Loading...</>}>
-      <MDXRemote
-        source={jsonData}
-        options={{
-          mdxOptions: {
-            remarkPlugins: [remarkGfm],
-          },
-        }}
-      />
-    </Suspense>
-  );
+    const jsonData = await res.text();
+
+    return (
+      <Suspense fallback={<>Loading...</>}>
+        <MDXRemote
+          source={jsonData}
+          options={{
+            mdxOptions: {
+              remarkPlugins: [remarkGfm],
+            },
+          }}
+        />
+      </Suspense>
+    );
+  } catch (error) {
+    console.log(error);
+  }
 }
