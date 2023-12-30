ec9f44708014273fbb5fb588152af59ed79c0824
IMHOJEONG <39ghwjd@naver.com>
2023-12-30 13:24:41 +0900

    [revise]: revise app.module.ts for deploy

--- apps/pwrcode-backend/src/app.module.ts
+++ apps/pwrcode-backend/src/app.module.ts
@@ -10,7 +10,8 @@ import { SearchModule } from './modules/search/search.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['.env.development', '.env.production'],
+      // envFilePath: ['.env.development', '.env.production'],
+      envFilePath: [(process.env.NODE_ENV === 'production') ? '.env.production' :  '.env.development'],
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
